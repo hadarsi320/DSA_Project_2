dba1435b240ff6d1b456d3ecc059ff164006e3a3
Hadar <hadarsi@campus.technion.ac.il>
2020-01-18 19:08:50 +0200

    finished testing and creating basic functions for Dynamic_Graph.h

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@
 
 int main()
 {
-    Dynamic_Graph dynamicGraph;
-    graph = dynamicGraph.Insert_Node(0);
+    Dynamic_Graph *dynamicGraph = new Dynamic_Graph();
+    Graph_Node *node0 = dynamicGraph->Insert_Node(0);
+    Graph_Node *node1 = dynamicGraph->Insert_Node(1);
+    Graph_Edge *edge0 = dynamicGraph->Insert_Edge(node0, node1);
+
+    dynamicGraph->Delete_Node(node0);
+    dynamicGraph->Delete_Edge(edge0);
+    dynamicGraph->Delete_Node(node1);
+    delete dynamicGraph;
+    int x = 1;
 }
